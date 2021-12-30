152afbab085f3bb304fdb14cf661210c79917fab
Maicol Battistini <maicolbattistini@live.it>
2021-12-30 17:48:28 +0100

    fix: 🐛 Replace anche con apici

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+/* eslint-disable unicorn/prefer-module,import/no-extraneous-dependencies,no-undef */
+// noinspection ES6ConvertRequireIntoImport
+
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 
@@ -5,8 +8,8 @@ async function replaceImport() {
   gulp.src(["./dist/index.js"], {
     allowEmpty: true
   })
-    .pipe(replace('from "openstamanager"', 'from "../../../index.js"'))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(replace(/(?<osm>from ["']openstamanager["'])/, "from '../../../index.js'"))
+    .pipe(gulp.dest('./dist/'));
 }
 
 exports.default = replaceImport;
